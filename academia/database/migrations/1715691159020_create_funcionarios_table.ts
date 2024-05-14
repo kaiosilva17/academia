268586a0d5d29cf914dad96d3b26010f08c56067
Kaio Victor da Silva <kvictor1711@gmail.com>
2024-05-14 10:20:20 -0300

    add models and controllers, make migrations

---
+++ academia/database/migrations/1715691159020_create_funcionarios_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'funcionarios'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('nome', 45).notNullable()
+      table.string('cargo', 45)
+      table.decimal('salario', 10, 2)
+      table.date('data_contratacao').notNullable()
+      table.integer('academia_id').unsigned().references('id').inTable('academias').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
