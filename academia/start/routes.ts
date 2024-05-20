b2f9ca53f6fe6b4d1fb8ce88469a8e763dbb68e0
Kaio Victor da Silva <kvictor1711@gmail.com.br>
2024-05-20 19:57:20 -0300

    CRUD

--- academia/start/routes.ts
+++ academia/start/routes.ts
@@ -8,9 +8,26 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AcademiasController from '../app/controllers/academias_controller.js'
+import ClientesController from '../app/controllers/clientes_controller.js'
+import CobrancasController from '../app/controllers/cobrancas_controller.js'
+import EquipamentosController from '../app/controllers/equipamentos_controller.js'
+import FuncionariosController from '../app/controllers/funcionarios_controller.js'
+import PagamentosController from '../app/controllers/pagamentos_controller.js'
+import PlanosController from '../app/controllers/planos_controller.js'
+import TreinosController from '../app/controllers/treinos_controller.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.resource('/academias', AcademiasController).apiOnly()
+router.resource('/clientes', ClientesController).apiOnly()
+router.resource('/cobrancas', CobrancasController).apiOnly()
+router.resource('/equipamentos', EquipamentosController).apiOnly()
+router.resource('/funcionarios', FuncionariosController).apiOnly()
+router.resource('/pagamentos', PagamentosController).apiOnly()
+router.resource('/planos', PlanosController).apiOnly()
+router.resource('/treinos', TreinosController).apiOnly()
