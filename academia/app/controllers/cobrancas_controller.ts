e40e6abc0594c7c4a14e1d639eccda3ca547dc14
Kaio Victor da Silva <kvictor1711@gmail.com.br>
2024-06-05 21:10:23 -0300

    aaaaaaaaaaaaa

--- academia/app/controllers/cobrancas_controller.ts
+++ academia/app/controllers/cobrancas_controller.ts
@@ -15,14 +15,14 @@ export default class CobrancasController {
     }
 
     async store({ request }: HttpContext) {
-        const dados = request.only(['id', 'valor', 'data_vencimento', 'status','cliente_Id'])
+        const dados = request.only(['id', 'valor', 'data_vencimento', 'status','clienteId'])
         return await Cobranca.create(dados)
     }
 
     async update({ params, request }: HttpContext) {
 
         const cobranca = await Cobranca.findOrFail(params.id)
-        const dados = request.only(['id', 'valor', 'data_vencimento', 'status','cliente_Id'])
+        const dados = request.only(['id', 'valor', 'data_vencimento', 'status','clienteId'])
 
         cobranca.merge(dados)
         return await cobranca.save()
--- academia/database/seeders/7_cobranca_seeder.ts
+++ academia/database/seeders/7_cobranca_seeder.ts
@@ -5,9 +5,9 @@ export default class extends BaseSeeder {
   async run() {
     // Write your database queries inside the run method
   await Cobranca.createMany([
-    {id: 1, valor:100, data_vencimento:20240515,status: 'Pendente', clienteId: 1},
-    {id: 2, valor:200, data_vencimento:20240515,status: 'Pendente', clienteId: 2},
-    {id: 3, valor:300, data_vencimento:20240515,status: 'Paga', clienteId: 3},
+    {id: 1, valor:100, data_vencimento:"20240515",status: 'Pendente', clienteId: 1},
+    {id: 2, valor:200, data_vencimento:"20240515",status: 'Pendente', clienteId: 2},
+    {id: 3, valor:300, data_vencimento:"20240515",status: 'Paga', clienteId: 3},
   ])
   }
 }
